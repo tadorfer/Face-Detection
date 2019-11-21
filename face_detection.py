4a6308b3501522e087932283f01f735d475dcca6
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2019-11-21 13:35:01 +0100

    Minor code adjustments

--- face_detection.py
+++ face_detection.py
@@ -1,35 +1,45 @@
 import cv2
 import sys
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def facedetection():
+    # loading classifier for frontal face
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-video_capture = cv2.VideoCapture(0)
+    # return video from default web cam
+    cap = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    if ret != 0 :
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # if there is video
+        if ret != 0: 
+            # convert to gray scale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+            # return rectangle with coordinates (x, y, w, h)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.2,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-           cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 100), 4)
+            # Draw rectangle around faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 4) # color order --> BGR
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
+            # Display resulting frame
+            cv2.imshow('Video', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # press 'q' to exit the while loop
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+    # Release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    facedetection()
